28df4fdba0749b6ba3303360970a4ddcf7f56956
Srijan Oraon <144728896+srijan-op@users.noreply.github.com>
2025-06-28 22:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import streamlit as st
 from faq_bot import JupiterFAQBot
 from llm_only_bot import LLMOnlyFAQBot
